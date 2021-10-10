5ba2760bb63e2d6f435c8460a331b38748e56bf7
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-10-10 13:51:11 -0400

    Add(ConsoleSample)

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleSample.InjectedClasses;
+using InjectStatic;
+
+namespace ConsoleSample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            App.Log("Called From Generated File");
+        }
+    }
+
+    [InjectStatic(typeof(Console))]
+    [InjectStatic(typeof(Logger))]
+    public static partial class App
+    {
+    }
+}
+
+namespace ConsoleSample.InjectedClasses
+{
+    internal class Console
+    {
+        public void WriteLine(string value, int amount)
+        {
+            for(var i = 0; i < amount; i++)
+            {
+                System.Console.WriteLine(value);
+            }
+        }
+
+        public string? ReadLine()
+        {
+            return System.Console.ReadLine();
+        }
+    }
+
+    internal class Logger
+    {
+        public void Log(string value)
+        {
+            System.Console.WriteLine(value);
+        }
+
+        public void LogLine(string value)
+        {
+            this.Log($"{value}\n");
+        }
+    }
+}
